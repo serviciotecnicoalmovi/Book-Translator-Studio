00a2512b4a6dcf541497841f810933dfef4e8298
serviciotecnicoalmovi <serviciotecnicoalmovi@gmail.com>
2026-08-06 04:54:28 -0400

    Fix minimal translation workspace compilation

--- src/BookTranslatorStudio.App/ViewModels/MainWindowViewModel.cs
+++ src/BookTranslatorStudio.App/ViewModels/MainWindowViewModel.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Windows;
